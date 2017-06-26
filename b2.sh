d857c5b097caba0f8f3a16b875e29a3bd6644808
Weiqi Ma <wma@menusifu.com>
2017-06-26 14:35:02 +0000

    ok

--- b2.sh
+++ b2.sh
@@ -3,5 +3,5 @@ mongo=$1
 dataDb=$2
 sudo docker  cp b1.sh $mongo:/tmp
 sudo docker exec -it $mongo /tmp/b1.sh $2
-sudo docker  cp  mongo:/tmp/$2.tar.gz .
+sudo docker  cp  $mongo:/tmp/$2.tar.gz .
 echo "done!"
