be3174a3863ca7dfa070d72f5b2e95e3cab56ee2
Weiqi Ma <wma@menusifu.com>
2017-06-26 14:36:07 +0000

    ok

--- backup2.sh
+++ backup2.sh
@@ -5,5 +5,5 @@ dataDb=$2
 tar=$(date +"%Y%m%d-%H%M%S$dataDb").tar.gz
 sudo docker  cp backup1.sh $mongo:/tmp
 sudo docker exec -i $mongo /tmp/backup1.sh $dataDb $tar
-sudo docker  cp  mongo:/tmp/$tar .
+sudo docker  cp  $mongo:/tmp/$tar .
 echo "done!"
